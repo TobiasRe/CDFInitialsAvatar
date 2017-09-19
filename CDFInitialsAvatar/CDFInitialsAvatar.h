cb173481f71b198ececcd70444d476c5365727d2
Tobias Reinsch <tobiasreinsch@gmail.com>
2017-09-19 14:17:24 +0200

    Added Framework Build

---
+++ CDFInitialsAvatar/CDFInitialsAvatar.h
@@ -0,0 +1,19 @@
+//
+//  CDFInitialsAvatar.h
+//  CDFInitialsAvatar
+//
+//  Created by  Tobias Reinsch on 19.09.17.
+//  Copyright © 2017 Toto Tvalavadze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CDFInitialsAvatar.
+FOUNDATION_EXPORT double CDFInitialsAvatarVersionNumber;
+
+//! Project version string for CDFInitialsAvatar.
+FOUNDATION_EXPORT const unsigned char CDFInitialsAvatarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CDFInitialsAvatar/PublicHeader.h>
+
+
